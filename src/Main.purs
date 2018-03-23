936c1f237ddb0021c5c5fefeff2d16bf13e8afe1
Harmen <alicebob@lijzij.de>
2018-03-23 17:42:35 +0100

    static rendering part works

--- src/Main.purs
+++ src/Main.purs
@@ -2,27 +2,28 @@ module Main where
 
 import Control.Monad.Eff (Eff)
 import Prelude
+import Data.Array as A
 import React.DOM as R
 import React.DOM.Props as RP
 import Thermite as T
 import DOM (DOM) as DOM
+import Data.Maybe (Maybe(..))
+import Data.String as Str
 
--- import Sudoku as S
+import Sudoku as S
 
 
 data Action = Increment | Decrement
 
--- type State = { -- sudoku :: S.Sudoku
-             -- counter :: Int
-             -- }
-type State = { counter :: Int }
+type State = { sudoku :: S.Sudoku
+             , counter :: Int
+             }
 
 initialState :: State
--- initialState = { sudoku: S.example, counter: 0 }
-initialState = { counter: 0 }
+initialState = { sudoku: S.example, counter: 0 }
 
 render :: T.Render State _ Action
-render dispatch _ state _ =
+render dispatch a state b =
   [ R.p' [ R.text "Value: "
          , R.text $ show state.counter
          ]
@@ -31,6 +32,7 @@ render dispatch _ state _ =
          , R.button [ RP.onClick \_ -> dispatch Decrement ]
                     [ R.text "Decrement" ]
          ]
+  , R.div' $ renderS dispatch a state.sudoku b
   ]
 
 performAction :: T.PerformAction _ State _ Action
@@ -42,3 +44,31 @@ spec = T.simpleSpec performAction render
 
 main :: Eff (dom :: DOM.DOM) Unit
 main = T.defaultMain spec initialState unit
+
+
+renderS :: T.Render S.Sudoku _ Action
+renderS dispatch _ sudoku _ =
+    [
+        R.div [RP.className "sudoku"] $ map (
+            \row -> R.div' $ map renderCell row
+        ) $ S.asLists sudoku
+    ]
+    where
+    renderCell c =
+        let content = case c.content of
+                            Nothing -> R.text ""
+                            Just (S.Clue i) -> R.text $ show i
+                            Just (S.Guess i) -> R.text $ show i
+            isClue = case c.content of
+                            Just (S.Clue _) -> true
+                            _ -> false
+            cl :: Array String
+            cl = A.catMaybes
+                [ if c.borders.north then Just "bnorth" else Nothing
+                , if c.borders.east then Just "beast" else Nothing
+                , if c.borders.south then Just "bsouth" else Nothing
+                , if c.borders.west then Just "bwest" else Nothing
+                , if isClue then Just "clue" else Nothing
+                ]
+        in
+            R.div [RP.className (Str.joinWith " " cl)] [ content ]
--- src/Sudoku.purs
+++ src/Sudoku.purs
@@ -2,29 +2,82 @@ module Sudoku
     ( Sudoku (..)
     , Cell(..)
     , example
+    , asLists
     )
 where
 
+import Prelude
 import Grid as G
 import Data.Map as M
+import Data.Array as A
 import Data.Tuple (Tuple (Tuple))
+import Data.Maybe (Maybe(..))
+import Data.String as Str
+import Data.Int as I
 
 data Cell
-    = Unknown
-    | Clue Int
+    = Clue Int
     | Guess Int
 
 type Sudoku = G.Grid Cell
 
+type Loc = Tuple Int Int
+
+
+type Borders = { north :: Boolean, east :: Boolean, south :: Boolean, west :: Boolean }
+
+type RenderCell =
+    { loc :: Loc
+    , borders :: Borders
+    , content :: Maybe Cell
+    }
+
+
 example :: Sudoku
-example = 
-    let m = M.fromFoldable [
-        Tuple (Tuple 0 0) (Clue 5)
-        , Tuple (Tuple 1 0) (Clue 3)
-        , Tuple (Tuple 0 1) (Clue 6)
-        , Tuple (Tuple 8 8) (Clue 9)
-        ]
+example = parse 9 """
+53..7....
+6..195...
+.98....6.
+8...6...3
+4..8.3..1
+7...2...6
+.6....28.
+...419..5
+....8..79
+"""
+
+parse :: Int -> String -> Sudoku
+parse n s =
+    let mlines = map Str.trim $ Str.split (Str.Pattern "\n") s
+        lines :: Array String
+        lines = A.filter (\l -> Str.length l == n) mlines
+        readCell x y c =
+            case I.fromString $ Str.singleton c of
+                Just i -> Just (Tuple (Tuple x y) (Clue i))
+                _ -> Nothing
+        clues = A.catMaybes $ A.concat $ A.mapWithIndex (\y row ->
+                    A.mapWithIndex (\x c -> readCell x y c) $ Str.toCharArray row
+            ) lines    
+        m = M.fromFoldable clues
     in
-        { dim: 9
+        { dim: n
         , cells: m
         }
+
+asLists :: Sudoku -> Array (Array RenderCell)
+asLists s =
+    let cell x y =
+            { loc : loc
+            , borders :
+                { north : y == 0 || mod y 3 == 0
+                , east : x == s.dim-1
+                , south : y == s.dim-1
+                , west : mod x 3 == 0
+                }    
+            , content : M.lookup loc s.cells
+            }
+            where
+                loc = Tuple x y
+        r = A.range 0 (s.dim - 1)
+    in
+        map (\y -> map (\x -> cell x y) r) r
